b9b6ed5bf58a3dce83706f1f7fc3a22348e6cb21
Madhusudhan Srinivasa <madhums8@gmail.com>
2018-11-30 11:32:16 +0100

    add demo

--- examples/example_0.js
+++ examples/example_0.js
@@ -2,10 +2,15 @@ $(function() {
     // Setup leaflet map
     var map = new L.Map('map');
 
-    var basemapLayer = new L.TileLayer('http://{s}.tiles.mapbox.com/v3/github.map-xgq2svrz/{z}/{x}/{y}.png');
-
+    var basemapLayer = new L.TileLayer('https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoibm9tYWRkZXYiLCJhIjoiY2pwMzJicmNjMGRoZzNxbW5saGF5d2t2eCJ9.SIa-W0ZxIqU726w6KJAw4Q');
+    
+    let json = window.trekker.filter(d => d.time).map(d => ({
+        time: d.time * 1000,
+        lat: Number(d.lat),
+        lng: Number(d.lng)
+    }))
     // Center map and default zoom level
-    map.setView([44.61131534, -123.4726739], 9);
+    map.setView([json[0].lat, json[0].lng], 17);
 
     // Adds the background layer to the map
     map.addLayer(basemapLayer);
@@ -20,7 +25,18 @@ $(function() {
         dateControl: true,
         sliderControl: true     
     };
-        
+    let coordinates = json.map(coord => [coord.lng, coord.lat]);
+    let times = json.map(coord => coord.time);
+    const data = {
+        "type": "Feature",
+        "geometry": {
+            "type": "MultiPoint",
+            "coordinates": coordinates
+        },
+        "properties": {
+            "time": times
+        }
+    }    
     // Initialize playback
-    var playback = new L.Playback(map, demoTracks, null, playbackOptions);   
+    var playback = new L.Playback(map, data, null, playbackOptions);   
 });
